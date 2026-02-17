7e96a4552e52fa02b2d63fbddec4828001741f4f
Asim Poudel <asimdkt63@gmail.com>
2026-02-17 21:41:45 +0545

    Add app.jsx

--- grocery-bud/src/App.jsx
+++ grocery-bud/src/App.jsx
@@ -1,12 +1,22 @@
 import Items from "./components/Items";
 import { groceryItems } from "./data/groceryItems";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { nanoid } from "nanoid";
+import Form from "./components/Form";
 import "./App.css";
 
 const App = () => {
   const [items, setItems] = useState(groceryItems);
+  const [editId, setEditId] = useState(null);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (editId && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editId]);
 
   const editCompleted = (itemId) => {
     const newItems = items.map((item) => {
@@ -24,13 +34,44 @@ const App = () => {
     toast.success("item deleted");
   };
 
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    toast.success("grocery item added");
+  };
+
+  const updateItemName = (newName) => {
+    const newItems = items.map((item) => {
+      if (item.id === editId) {
+        return { ...item, name: newName };
+      }
+      return item;
+    });
+    setItems(newItems);
+    setEditId(null);
+    toast.success("item updated");
+  };
+
   return (
     <section className="section-center">
       <ToastContainer position="top-center" />
+      <Form
+        addItem={addItem}
+        updateItemName={updateItemName}
+        editItemId={editId}
+        itemToEdit={items.find((item) => item.id === editId)}
+        inputRef={inputRef}
+      />
       <Items
         items={items}
         editCompleted={editCompleted}
         removeItem={removeItem}
+        setEditId={setEditId}
       />
     </section>
   );
